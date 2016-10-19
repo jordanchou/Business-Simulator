043ef3315dd5bc79d5393e7b7949261aa756907f
jchoux <jordan.chou1@gmail.com>
2016-10-19 22:16:08 +0800

    Added getEvent() for EventFactory

--- src/main/controller/factory/EventFactory.java
+++ src/main/controller/factory/EventFactory.java
@@ -22,39 +22,42 @@ public class EventFactory
     public Event getEvent(String line)
     {
         String[] lineArray;
-        Event event;
+        Event event = null;
+        long year;
 
         lineArray = line.split(",");
+
+        year = Long.parseLong(lineArray[0]);
 
         switch (lineArray[1])
         {
             case "R-":
-
+                event = new RevenueDecreaseEvent(year, lineArray[2]);
                 break;
 
             case "R+":
-
+                event = new RevenueIncreaseEvent(year, lineArray[2]);
                 break;
 
             case "W+":
-
+                event = new WageIncreaseEvent(year);
                 break;
 
             case "W-":
-
+                event = new WageDecreaseEvent(year);
                 break;
 
             case "V+":
-
+                event = new ValueIncreaseEvent(year, lineArray[2]);
                 break;
 
             case "V-":
-
+                event = new ValueDecreaseEvent(year, lineArray[2]);
                 break;
+            default:
+                //throw exception!
         }
 
-
-
-        return null;
+        return event;
     }
 }
