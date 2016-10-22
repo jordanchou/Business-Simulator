d4af8cd06fde7c98b3f75e480f0ff65c10675053
jchoux <jordan.chou1@gmail.com>
2016-10-22 22:12:51 +0800

    Modified PropertyReader

--- src/main/java/controller/reader/PropertyReader.java
+++ src/main/java/controller/reader/PropertyReader.java
@@ -34,13 +34,15 @@ public class PropertyReader extends Reader
         switch (lineArray[1])
         {
             case "C":
-                property = new Company(lineArray[0], (Company)properties.getProperty(lineArray[2]), Double.parseDouble(lineArray[3]), new BankAccount());
+                property = new Company(lineArray[0], (Company)properties.getProperty(lineArray[2]),
+                                        Double.parseDouble(lineArray[3]), new BankAccount());
 
                 break;
 
             case "B":
-                property = new BusinessUnit(lineArray[0], (Company)properties.getProperty(lineArray[2]), Double.parseDouble(lineArray[3]),
-                                            Double.parseDouble(lineArray[4]), Double.parseDouble(lineArray[5]));
+                property = new BusinessUnit(lineArray[0], (Company)properties.getProperty(lineArray[2]),
+                                            Double.parseDouble(lineArray[3]), Double.parseDouble(lineArray[4]),
+                                            Double.parseDouble(lineArray[5]));
                 break;
 
             default:
